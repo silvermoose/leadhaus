91c685a7e3446efcf854efac3c03773d4bb7a507
silvermoose <femat.prime@gmail.com>
2016-01-30 23:49:35 -0800

    updates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
     winston = require('winston')
 
 var conString = "postgres://yuiztbkchhoqop:oKokli-_a1s9mRzO3TmnDC0VDH@ec2-54-227-249-166.compute-1.amazonaws.com:5432/d1i361s4l12e55";
-//app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -40,12 +40,9 @@ app.post('/register', function(req, res) {
     auth.signup;
 });
 
-/*
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-*/
-app.listen(5000);
-console.log('server ready')
 
 
